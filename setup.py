2aa349550cd4d2323afb1d85858566313ade5e67
syntaxaire <syntaxaire@gmail.com>
2019-09-22 17:08:14 -0400

    Add external pillow dependency

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/syntaxaire/hagadias",
     packages=setuptools.find_packages(),
+    install_requires=['pillow'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
