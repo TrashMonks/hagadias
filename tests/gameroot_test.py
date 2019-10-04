17aefd434d01d74f07a963c8b5308397a91ff5bb
syntaxaire <syntaxaire@gmail.com>
2019-10-04 08:43:03 -0400

    Add test for game version attribute

--- tests/gameroot_test.py
+++ tests/gameroot_test.py
@@ -7,3 +7,12 @@ from hagadias.gameroot import GameRoot
 
 def test_gameroot(gameroot):
     assert isinstance(gameroot, GameRoot)
+
+
+def test_gamever(gameroot):
+    # game versions are strings like '2.0.194.1'
+    ver = gameroot.gamever
+    assert isinstance(ver, str)
+    assert len(ver) > 4
+    assert ver[0] in '01234567890'
+    assert '.' in ver
