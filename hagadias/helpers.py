5ea26c955af5274580e161a4093d53ea7d173604
syntaxaire <syntaxaire@gmail.com>
2019-10-04 14:16:01 -0400

    Switch to cross-platform method for finding DLL version strings

--- hagadias/helpers.py
+++ hagadias/helpers.py
@@ -5,6 +5,8 @@ import re
 from ctypes import *
 
 # load and store the Code Page 437 to Unicode translation
+import pefile
+
 CP437_MAP_FILE = os.path.join(os.path.dirname(__file__), 'IBMGRAPH.TXT')
 cp437_conv = {}
 with open(CP437_MAP_FILE) as f:
@@ -33,75 +35,22 @@ def cp437_to_unicode(val: int):
 
 
 # From https://stackoverflow.com/questions/580924/python-windows-file-version-attribute:
+def get_dll_version_string(path, throwaway):
+    """Return the version information from a PE file (exe or dll)."""
 
-# returns the requested version information from the given file
-#
-# `language` should be an 8-character string combining both the language and
-# codepage (such as "040904b0"); if None, the first language in the translation
-# table is used instead
-#
-def get_dll_version_string(filename, what, language=None):
-    # VerQueryValue() returns an array of that for VarFileInfo\Translation
-    #
-    class LANGANDCODEPAGE(Structure):
-        _fields_ = [
-            ("wLanguage", c_uint16),
-            ("wCodePage", c_uint16)]
-
-    wstr_file = wstring_at(filename)
-
-    # getting the size in bytes of the file version info buffer
-    size = windll.version.GetFileVersionInfoSizeW(wstr_file, None)
-    if size == 0:
-        raise WinError()
-
-    buffer = create_string_buffer(size)
-
-    # getting the file version info data
-    if windll.version.GetFileVersionInfoW(wstr_file, None, size, buffer) == 0:
-        raise WinError()
-
-    # VerQueryValue() wants a pointer to a void* and DWORD; used both for
-    # getting the default language (if necessary) and getting the actual data
-    # below
-    value = c_void_p(0)
-    value_size = c_uint(0)
-
-    if language is None:
-        # file version information can contain much more than the version
-        # number (copyright, application name, etc.) and these are all
-        # translatable
-        #
-        # the following arbitrarily gets the first language and codepage from
-        # the list
-        ret = windll.version.VerQueryValueW(
-            buffer, wstring_at(r"\VarFileInfo\Translation"),
-            byref(value), byref(value_size))
-
-        if ret == 0:
-            raise WinError()
-
-        # value points to a byte inside buffer, value_size is the size in bytes
-        # of that particular section
-
-        # casting the void* to a LANGANDCODEPAGE*
-        lcp = cast(value, POINTER(LANGANDCODEPAGE))
-
-        # formatting language and codepage to something like "040904b0"
-        language = "{0:04x}{1:04x}".format(
-            lcp.contents.wLanguage, lcp.contents.wCodePage)
-
-    # getting the actual data
-    res = windll.version.VerQueryValueW(
-        buffer, wstring_at("\\StringFileInfo\\" + language + "\\" + what),
-        byref(value), byref(value_size))
-
-    if res == 0:
-        raise WinError()
-
-    # value points to a string of value_size characters, minus one for the
-    # terminating null
-    return wstring_at(value.value, value_size.value - 1)
+    pe = pefile.PE(path)
+    if hasattr(pe, 'VS_VERSIONINFO'):
+        for idx in range(len(pe.VS_VERSIONINFO)):
+            if hasattr(pe, 'FileInfo') and len(pe.FileInfo) > idx:
+                for entry in pe.FileInfo[idx]:
+                    if hasattr(entry, 'StringTable'):
+                        for st_entry in entry.StringTable:
+                            for str_entry in sorted(list(st_entry.entries.items())):
+                                key = str_entry[0].decode('utf-8', 'backslashreplace')
+                                val = str_entry[1].decode('utf-8', 'backslashreplace')
+                                if key == 'ProductVersion':
+                                    return val
+    raise ValueError
 
 
 class DiceBag:
