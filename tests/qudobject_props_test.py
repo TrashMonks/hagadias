d9890bc8df7e8d3babffadc971e0fb58ba5b83e8
syntaxaire <syntaxaire@gmail.com>
2019-10-11 19:01:53 -0400

    Add AV and DV tests to aid with #4

--- tests/qudobject_props_test.py
+++ tests/qudobject_props_test.py
@@ -10,6 +10,29 @@ def test_strip_qud_color_codes():
 
 
 # Properties
+def test_av(qindex):
+    pairs = {'Chain Mail': '3',
+             'Stopsvaalinn': '3',
+             'Basalt': '10',
+             'Q Girl': '7',  # 4 from quills, 1 from polyhedral rings, 0 from powered exoskeleton,
+                             # 1 from ulnar stimulator, 1 from plastifer sneakers
+             'Warden Ualraig': '6',  # 2 from horn, 3 from chainmail, 1 from leather boots
+             'IrritableTortoise': '4'}
+    for obj, av in pairs.items():
+        assert qindex[obj].av == av
+
+
 def test_chargeused(qindex):
     obj = qindex['Geomagnetic Disc']
     assert obj.chargeused == '400'
+
+
+def test_dv(qindex):
+    pairs = {'Chain Mail': '-1',
+             'Stopsvaalinn': '0',
+             'Basalt': '-10',
+             'Q Girl': '6',
+             'Warden Ualraig': '8',
+             'IrritableTortoise': '4'}
+    for obj, dv in pairs.items():
+        assert qindex[obj].dv == dv
